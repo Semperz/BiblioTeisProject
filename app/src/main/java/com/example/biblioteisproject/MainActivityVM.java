75fa6dc6cf19b0a951bf3c528a4e4f9c4d9411af
a23sergiopg <serprigar@gmail.com>
2025-02-11 12:53:47 +0100

    Initial commit

---
+++ app/src/main/java/com/example/biblioteisproject/MainActivityVM.java
@@ -0,0 +1,21 @@
+package com.example.biblioteisproject;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.biblioteisproject.API.models.Book;
+import com.example.biblioteisproject.API.models.User;
+
+public class MainActivityVM extends ViewModel {
+
+    MutableLiveData<Book> book;
+    MutableLiveData<User> user;
+
+    public MainActivityVM(){
+        book = new MutableLiveData<>();
+        user = new MutableLiveData<>();
+    }
+
+
+
+}
