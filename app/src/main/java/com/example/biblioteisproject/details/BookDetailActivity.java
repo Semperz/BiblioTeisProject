8fb3821e2ea928653090be600ff7e6d3e6d05017
DavBaquero <david.baquero.19@gmail.com>
2025-02-13 09:05:21 +0100

    feat(BookDetailActivity): create view of details

---
+++ app/src/main/java/com/example/biblioteisproject/details/BookDetailActivity.java
@@ -0,0 +1,18 @@
+package com.example.biblioteisproject.details;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.biblioteisproject.R;
+
+public class BookDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_detail);
+    }
+
+
+}
