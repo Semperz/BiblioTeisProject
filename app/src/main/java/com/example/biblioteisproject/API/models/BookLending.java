05bc5bb6f37ca1e6c5786b0fdc5f50c1f61e524d
a23manuelcr <manuelcendonr@gmail.com>
2025-02-13 09:02:25 +0100

    feat(BookLending): fix teachers issue

--- app/src/main/java/com/example/biblioteisproject/API/models/BookLending.java
+++ app/src/main/java/com/example/biblioteisproject/API/models/BookLending.java
@@ -1,25 +1,11 @@
 package com.example.biblioteisproject.API.models;
 
 public class BookLending {
-    private int id;
+
     private int bookId;
     private int userId;
-    private String lendDate;
-    private String returnDate;
-
-    private User user;
-    private Book book;
 
     // Getters & Setters
-
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
 
     public int getBookId() {
         return bookId;
@@ -37,19 +23,4 @@ public class BookLending {
         this.userId = userId;
     }
 
-    public String getLendDate() {
-        return lendDate;
-    }
-
-    public void setLendDate(String lendDate) {
-        this.lendDate = lendDate;
-    }
-
-    public String getReturnDate() {
-        return returnDate;
-    }
-
-    public void setReturnDate(String returnDate) {
-        this.returnDate = returnDate;
-    }
 }
