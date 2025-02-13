b12b3924954ba8d8327c4dc69375bf97b3da2fec
a23sergiopg <serprigar@gmail.com>
2025-02-13 09:29:41 +0100

    refactor(BookListAdapter.java): refactor unused imports

--- app/src/main/java/com/example/biblioteisproject/books/AvailableBooks.java
+++ app/src/main/java/com/example/biblioteisproject/books/AvailableBooks.java
@@ -4,11 +4,8 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
--- app/src/main/java/com/example/biblioteisproject/books/BooksListAdapter.java
+++ app/src/main/java/com/example/biblioteisproject/books/BooksListAdapter.java
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.biblioteisproject.API.models.Book;
 import com.example.biblioteisproject.R;
+import com.example.biblioteisproject.details.BookDetailActivity;
 
 import java.util.List;
 
@@ -32,9 +33,9 @@ public class BooksListAdapter extends RecyclerView.Adapter {
         cardViewHolder.getTvAutor().setText(book.getAuthor());
         cardViewHolder.getTvFechaPubli().setText(book.getPublishedDate());
         cardViewHolder.getBtnDetalles().setOnClickListener((view) -> {
-           /* Intent intent = new Intent(view.getContext(), BookDetailActivity.class);
+           Intent intent = new Intent(view.getContext(), BookDetailActivity.class);
             intent.putExtra("book", books.get(position));
-            view.getContext().startActivity(intent);*/
+            view.getContext().startActivity(intent);
         });
     }
 
